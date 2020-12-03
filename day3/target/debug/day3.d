8f904652dc90f2ca287ef0a7bf6d08cafff76259
Naman Wahi <naman@nplan.io>
2020-12-03 19:42:44 +0000

    day 3

--- day3/target/debug/day3.d
+++
@@ -1,1 +0,0 @@
-/Users/namanwahi/advent-of-code/day3/target/debug/day3: /Users/namanwahi/advent-of-code/day3/src/main.rs
--- day3/target/debug/deps/day3.d
+++
@@ -1,5 +0,0 @@
-/Users/namanwahi/advent-of-code/day3/target/debug/deps/day3: src/main.rs
-
-/Users/namanwahi/advent-of-code/day3/target/debug/deps/day3.d: src/main.rs
-
-src/main.rs:
