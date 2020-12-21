aac29514c8e0b875e0af65e486dcbf76caa0d096
Naman Wahi <naman@nplan.io>
2020-12-21 17:28:24 +0000

    day 20

--- day20/solution.py
+++ day20/solution.py
@@ -28,12 +28,13 @@ def parse_data(lines):
 def transform(arr, x, y, rots):
     res = arr
 
-    res = np.rot90(res, k=rots)
-
     if x:
         res = np.flip(arr, 1)
     if y:
         res = np.flip(arr, 0)
+
+    res = np.rot90(res, k=rots)
+
     return res
 
 
@@ -55,12 +56,6 @@ def get_all_transformations(tile):
         for rots in [0, 1, 2, 3]:
             res.append((transform(tile, x_flip, y_flip, rots), x_flip, y_flip, rots))
     return res
-
-def get_all_transformation_args():
-    for x_flip in [False, True]:
-        for y_flip in [True, False]:
-            for rots in [0, 1, 2, 3]:
-                yield (x_flip, y_flip, rots)
 
 def adjacent_tiles(edges):
     full_connections = {}
@@ -89,7 +84,6 @@ def adjacent_tiles(edges):
             res[tid].add(other_tid)
 
     return res
-
 
 
 def reassemble(adjacent):
@@ -130,6 +124,46 @@ def reassemble(adjacent):
 
     return _reassemble(0, tile_assignments, all_tids)
 
+def stich(tiles, tile_positions):
+    all_tids = tiles.keys()
+    num_tiles = len(all_tids)
+    num_tiles_edge = int(round(num_tiles ** 0.5))
+    transformed_tiles = np.repeat(None, num_tiles).reshape((num_tiles_edge, num_tiles_edge))
+
+    pos_to_coord = lambda x: (x // num_tiles_edge, x % num_tiles_edge)
+
+    def _stich(pos, transformed_tiles):
+        if pos == num_tiles:
+            return transformed_tiles
+
+        (i, j) = pos_to_coord(pos)
+        for tile, _, _, _ in get_all_transformations(tiles[tile_positions[i][j]]):
+            if j > 0: # check left
+                left_tile = transformed_tiles[i, j - 1]
+                assert left_tile is not None
+                if not (left_tile[:, -1] == tile[:, 0]).all():
+                    continue
+
+            if i > 0: # check top
+                above_tile = transformed_tiles[i - 1, j]
+                assert above_tile is not None
+                if not (above_tile[-1, :] == tile[0, :]).all():
+                    continue
+
+            new_transformed_tiles = transformed_tiles.copy()
+            new_transformed_tiles[i, j] = tile
+            res = _stich(pos + 1, new_transformed_tiles)
+            if res is not None:
+                return res
+
+        return None
+
+    return np.array(_stich(0, transformed_tiles).tolist())
+
+def show_tile(tile):
+    for row in tile:
+        print("".join(row))
+
 
 if __name__ == "__main__":
     with open("inputs.txt", "r") as f:
@@ -139,10 +173,33 @@ if __name__ == "__main__":
 
     edges = tile_to_edges(tiles)
 
-    print("Calculating adjancent")
     adjacent = adjacent_tiles(edges)
 
-    print("Reassembling")
-    res = reassemble(adjacent)
+    tile_positions = reassemble(adjacent)
 
-    print(res[0][0] * res[0][-1] * res[-1][0] * res[-1][-1])
+    print("Part 1:" tile_positions[0][0] * tile_positions[0][-1] * tile_positions[-1][0] * tile_positions[-1][-1])
+
+    stiched_image = stich(tiles, tile_positions)
+
+    tile_width = stiched_image.shape[-1]
+
+    stiched_image = np.concatenate(np.concatenate(stiched_image[:,:,1:-1,1:-1], axis=1), axis=1)
+
+    sea_monster = np.array([
+        list("                  # "),
+        list("#    ##    ##    ###"),
+        list(" #  #  #  #  #  #   "),
+    ])
+
+    for stiched_image, _, _, _ in get_all_transformations(stiched_image):
+        for i in range(stiched_image.shape[0]):
+            for j in range(stiched_image.shape[1]):
+                search_region = stiched_image[i:i+sea_monster.shape[0], j:j+sea_monster.shape[1]]
+                if sea_monster.shape == search_region.shape:
+                    monster_mask = sea_monster == "#"
+                    region_mask = np.logical_or(search_region == "#", search_region == "O")
+
+                    if np.logical_and(monster_mask, region_mask).sum() == monster_mask.sum():
+                        search_region[monster_mask] = "O"
+
+        print("Part 2:",np.logical_or(stiched_image == "O", stiched_image == "#").sum() - (stiched_image == "O").sum())
